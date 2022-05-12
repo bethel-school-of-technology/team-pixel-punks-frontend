f33ec646137289180f0f54d44ad19f7a84385576
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-05-11 18:53:57 -0600

    zip to weather API calls updated
    I'm able to pass in a zipcode and receive the weather data for the location.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import API from './Services/API';
 
 function App() {
 
-
-  //await promise, then console log the next day's high and low temperature to the console.
-  //more data is available in the raw data, we can include more if necessary/wanted.
-  const testWeatherAPI = API.getWeatherData('38','-104').then(res => {
-    console.log(res.data.daily[0].temp.max + " degrees F high temp");
-    console.log(res.data.daily[0].temp.min + " degrees F low temp");
-  });
+  //this function first gets the latitude and longitude via API for a passed in zipcode, then sends the location
+  //to another function that gets the weather data via API for the location passed in.
   
+  const zipToLocation = (zip) => {
+    API.getLatLong(zip).then(res => {
+
+      let latitude = res.data.data[0].latitude.toString();
+      let longitude = res.data.data[0].longitude.toString();
+      console.log(latitude + " " + longitude);
+
+      const getLocationWeather = API.getWeatherData(latitude, longitude).then(res => {
+        console.log(res.data.daily[0].temp.max + " degrees F high temp");
+        console.log(res.data.daily[0].temp.min + " degrees F low temp");
+      });
+    })
+  };
+
+  //test code for weather API calls  unsure why the console logs are executing twice each time.
+  zipToLocation(90210);
+
+
 
   return (
     <div className="App">
--- src/Services/API.js
+++ src/Services/API.js
@@ -9,8 +9,13 @@ const API = {
     + lat 
     + '&lon=' + long 
     + '&exclude=hourly,minutely,alerts&units=imperial&appid=58d0361f438e67f6c23c40e7d62a5c84');
+  },
+  getLatLong: (zip) => {
+    return axios.get
+    ('http://api.positionstack.com/v1/forward?access_key=3f10f040b0272fd0c5f95ea64844da9e&query=' 
+    + zip);
   }
 
-}
+};
 
 export default API;
