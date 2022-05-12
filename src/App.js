52eaa8c99fbe0d1c425d682f967a0a62338cd6a2
Patrick Ruggeri <patrickofca@gmail.com>
2022-05-11 17:38:22 -0700

    Reworked Login/Signup to use Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
-//THIS HAS NOT BEEN CODED YET. NEED ROUTES AND VARIABLES - PATRICK
-
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './Components/login.component'
+import SignUp from './Components/signup.component'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              Hot and Frosty
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './screens/Login';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
-const Index = ({ pathname }) => {
-  //Started switch case here for navbar
-  switch (pathname) {
-    //for login page - needs form coded in
-    case '/login':
-
-  //Other pages go here with more switch cases
-
-
- 
-  //Need to code in a home page or city page as default
-  default:
-    return <Home />
-  }
-};
-
-let pathname = window.location.pathname;
-
-
-//Cannot remember if I used "root" or "login" in index.html
-ReactDOM.render(<Index pathname={pathname} />, document.getElementById('root'));
-
-//Need to add click event for button later
-window.addEventListener( 'popstate', () => {
-  pathname = window.location.pathname;
-});
-
+reportWebVitals()
