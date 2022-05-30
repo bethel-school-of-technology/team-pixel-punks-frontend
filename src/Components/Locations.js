92d8457c7a2a0dc08ca1076fa3aed32a72c29cb6
Patrick Ruggeri <patrickofca@gmail.com>
2022-05-30 05:13:34 -0700

    Working on Cities page

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,70 +1,46 @@
 
-import React, { Component, onChange, onClick } from 'react';
+import React, { Locations, onChange, onClick, } from 'react';
 //import API from '../Services/API';
 //import axios from 'axios';
-import React, { useState } from 'react';
-import API from '../Services/API';
+//import React, { useState } from 'react';
+//import API from '../Services/API';
+//import { ReactDOM } from 'react';
+
 
 
 // const Locations = () => {
-    //console.log(localStorage.getItem("token"));
+//     console.log(localStorage.getItem("token"));
 //     const [user, setUser] = useState({
 //         email: "",
 //         firstName: "",
 //         lastName: ""
-//       });
+//     });
 
-//       API.getUserLocations().then(res =>{
-          //console.log(res.body);
-        //   setUser.email = res.body.Email
-        //   setUser.firstName = res.body.FirstName
-        //   setUser.lastName = res.body.LastName
-      })
-
-
-
-
-
-
-
-//console.log(userLocations);
-//     return (
-//         <div className="App" >
-//             <table>
-//                 <tbody>
-//                     <tr>
-//                         <th>City Name</th>
-//                         <th>Frost Alert</th>
-//                         <th>Sun Alert</th>
-//                     </tr>
-//                     <tr>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//         </div>
-//     );
-// }
+//     API.getUserLocations().then(res => {
+//         console.log(res.body);
+//         setUser.email = res.body.Email
+//         setUser.firstName = res.body.FirstName
+//         setUser.lastName = res.body.LastName
+//     })
+// };
 
 
 
 
 //getCity = "a List of Cities";
 
-const Locations = React.createClass({
+const getCity = React.createClass({
     render: function () {
-        var cities = [];
+        let getCity = [ 'Dallas', 'Redding', 'San Jose'];
         // json object goes into array
-        return (
-            <>
-                <ul>
-                    {cities.map(function (city, index) {
-                        return <li key={index}> {city} </li>;
-                    }
-                    )}
-                </ul>
-            )
-        }
-    });
+        let getCityList = getCity.map(function (city) {
+            return <li> { city } </li>;
+        })
+        return <ul> { getCityList} </ul>
+    }
+});
+
+
 
                 // <h3>This is {this.myCitiesList} </h3>);
                 <section id="myCitiesList" className="list-group">
@@ -74,28 +50,30 @@ const Locations = React.createClass({
                             <button onClick={onClick}>{getCity}</button>
                         </div>
 
-                        <div className="container-lg">
+                        {/* <div className="container-lg">
                             <div className="text center">
                                 <h2>My Cities</h2>
                                 <p className="tagLine">Here is a list of your chosen cities.</p>
                                 <ul className="list-group">
-                                    <li className="list-group-item">Denver</li>
-                                    <li className="list-group-item">Detroit</li>
-                                    <li className="list-group-item">Pittsburgh</li>
-                                    <li className="list-group-item">Redding</li>
+                                    <li className="list-group-item">{( getCityList )}</li>
+                                    <li className="list-group-item">{(getCityList)}</li>
+                                    <li className="list-group-item">{(getCityList)}</li>
+                                    <li className="list-group-item">{(getCityList)}</li>
                                     <li className="list-group-item">Fresno</li>
                                 </ul>
 
 
                             </div>
 
-                        </div>
+                        </div> */}
 
 
 
                     </div>
-                </section></>
-        
-    
+                </section>
 
-export default Locations;
+                //ReactDOM.render(document.gerElementById('cities'));
+
+
+
+        export default Locations;
