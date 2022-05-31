c42a8eb625e77ce93571f674226f1c6252ad15b9
Patrick Ruggeri <patrickofca@gmail.com>
2022-05-30 18:23:53 -0700

    Locations work

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,28 +1,26 @@
 
-import React, { Locations, onChange, onClick, } from 'react';
-//import API from '../Services/API';
-//import axios from 'axios';
-//import React, { useState } from 'react';
-//import API from '../Services/API';
-//import { ReactDOM } from 'react';
+import React, { Locations, onChange, onClick, useState } from 'react';
+import axios from 'axios';
+import API from '../Services/API';
+import { ReactDOM } from 'react';
 
 
 
-// const Locations = () => {
-//     console.log(localStorage.getItem("token"));
-//     const [user, setUser] = useState({
-//         email: "",
-//         firstName: "",
-//         lastName: ""
-//     });
+const Locations = () => {
+    console.log(localStorage.getItem("token"));
+    const [user, setUser] = useState({
+        email: "",
+        firstName: "",
+        lastName: ""
+    });
 
-//     API.getUserLocations().then(res => {
-//         console.log(res.body);
-//         setUser.email = res.body.Email
-//         setUser.firstName = res.body.FirstName
-//         setUser.lastName = res.body.LastName
-//     })
-// };
+    API.getUserLocations().then(res => {
+        console.log(res.body);
+        setUser.email = res.body.Email
+        setUser.firstName = res.body.FirstName
+        setUser.lastName = res.body.LastName
+    })
+
 
 
 
@@ -32,22 +30,23 @@ import React, { Locations, onChange, onClick, } from 'react';
 const getCity = React.createClass({
     render: function () {
         let getCity = [ 'Dallas', 'Redding', 'San Jose'];
-        // json object goes into array
-        let getCity = getCity.map(function (city) {
-            return <li> { city } </li>;
+        console.log(getCity);
+        // json object of cities goes into array
+        let cityList = getCity.map(function (city,index) {
+            return <li key= { city }> </li>;
         })
-        return <ul> { getCityList} </ul>
+        
     }
 });
 
-
+    return (
 
                 // <h3>This is {this.myCitiesList} </h3>);
                 <section id="myCitiesList" className="list-group">
                     <div className="mb-3">
                         <div className="input">
                             <input type="text" onChange={onChange} />
-                            <button onClick={onClick}>{getCity}</button>
+                            <button onClick={onClick}>{cityList}</button>
                         </div>
 
                         {/* <div className="container-lg">
@@ -72,8 +71,10 @@ const getCity = React.createClass({
                     </div>
                 </section>
 
-                //ReactDOM.render(document.gerElementById('cities'));
+             ReactDOM.render(document.gerElementById('cities'));
 
+             )
 
+            }
 
         export default Locations;
