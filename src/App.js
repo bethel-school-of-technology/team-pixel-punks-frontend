1f103f336b908fa2fd99e0894c6b8bbf071355bc
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-06-07 11:52:51 -0600

    fixed error and added back img source

--- src/App.js
+++ src/App.js
@@ -11,30 +11,6 @@ import Logout from './Components/Logout';
 import UpdateCity from './Components/UpdateCity';
 
 
-
-//added by Elise - commenting out - in my test app i had this in my index.js not app.js
-//import ReactDOM from 'react-dom';
-//import frosty from './frosty.svg';
-//import sunny from './sunny.svg';
-//import './index.css';
-//const updateAlert = ( maxTemp, minTemp) => {
-//   if (maxTemp > '75' ) {
-//   return ( <img src = {sunny} className="App-logo" alt = "sunny"></img>);
-//   } else if (minTemp <33) {
-//     return (<img src = {frosty} className="App-logo" alt = "sunny"></img>);
-//   } else {
-//     return ('no alert');
-//   }
-//   };
-
-// ReactDOM.render( updateAlert (
-//     '80',
-//     '60',
-//   ),
-//   document.getElementById('root')
-// );
-
-
 function App() {
 
 
@@ -42,56 +18,58 @@ function App() {
     <Router>
       <div className="App">
         <nav className="navbar navbar-expand-sm navbar-light fixed-top">
-         <Link className="navbar-brand mb-0" to={'/login'}>
-              <img className="d-inline-block" id="navLogo">
-               </img>
-              
-            </Link>
-            <button type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNav" 
+          <Link className="navbar-brand mb-0" to={'/login'}>
+            <img className="d-inline-block" id="navLogo"
+              src={require("./snow-sun.png")}>
+            </img>
+
+          </Link>
+          <button type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
             className="navbar-toggler"
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>           
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/login'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/logout'}>
-                    Logout
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={'/login'}>
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/sign-up'}>
+                  Sign up
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={'/logout'}>
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
         </nav>
 
-        </div>
-        <div className="auth-wrapper">
+      </div>
+      <div className="auth-wrapper">
         <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/locations/" element={<Locations />} />
-              <Route path="/update-city/:id" element={<UpdateCity />} />
-            </Routes>
-          </div>
-
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/locations/" element={<Locations />} />
+            <Route path="/update-city/:id" element={<UpdateCity />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
 
   )
-}
+};
+
 export default App;
